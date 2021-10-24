09b157e0e3c011222b93e230b20b179a21b43384
Goldie <goldiejackson25@yahoo.com>
2021-10-23 19:28:07 -0500

    commit tree c81993b94ba3ef6d4fc1874747d5b36ca4a6b543

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,37 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+  <div className='navbar'>
+    <NavLink
+      to="/movies"
+      exact
+      activeStyle={{ outline: 'solid'}}
+    >
+      Movies
+    </NavLink>
+    <NavLink
+      to="/directors"
+      exact
+      activeStyle={{ outline: 'solid'}}
+    >
+      Directors
+    </NavLink>
+    <NavLink
+      to="/actors"
+      exact
+      activeStyle={{ outline: 'solid'}}
+    >
+      Actors
+    </NavLink>
+    <NavLink
+      to="/"
+      exact
+      activeStyle={{ outline: 'solid'}}
+    >
+      Home
+    </NavLink>
+  </div>)
 }
 
 export default NavBar;
